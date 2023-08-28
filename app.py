3787643f46530a53e4492058d38d5745d4864a26
rmakaren <makarenko100@gmail.com>
2023-08-28 09:10:52 +0200

    work in progress

--- app.py
+++ app.py
@@ -23,11 +23,19 @@ import numpy as np
 from autostats.preprocessing import Preprocess
 from autostats.autostats import GroupComparisonAnalyzer
 
-dataset = pd.read_csv("/media/rostyslav/Toshiba/Projects/autostats/01-data/AB_NYC_2019_for_visualisation.csv", sep=",")
+dataset = pd.read_csv("/media/rostyslav/Toshiba/Projects/genomic_vision/QC/2nd_analysis/data_for_analysis_prerop.csv", sep=",")
 
-preprocess = Preprocess(dataset=dataset, labels="neighbourhood_group")
+preprocess = Preprocess(dataset=dataset, labels="Statut_lamelle")
 
-dataset_prep = preprocess.preprocessing(dataset=dataset, labels="neighbourhood_group")
+# "Advancing", "ADV_Max-Min", "Receding", "REC_Max-Min", 
+# "DSMulti_level", "BC_level", "H_level_", "TS_DNA_fix_level", "Score_QC",
+# "Density", "Density_perc", "Waviness_level", "RCD_level",
+
+dataset = dataset[["CA", "Statut_lamelle"]]
+
+dataset_prep = preprocess.preprocessing(dataset=dataset, labels="Statut_lamelle")
 # dataset_prep2 = dataset_prep.drop("caa", axis=1)
-stats = GroupComparisonAnalyzer(dataset=dataset_prep, labels="neighbourhood_group", output_path="/media/rostyslav/Toshiba/Projects/autostats/02-output/")
+stats = GroupComparisonAnalyzer(dataset=dataset_prep, labels="Statut_lamelle", output_path="/media/rostyslav/Toshiba/Projects/genomic_vision/QC/2nd_analysis/")
 stats.run_group_comparison()
+
+# %%
